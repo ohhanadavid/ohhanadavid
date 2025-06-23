5f37f2649348f0766f5967c26a7c39dd3a957e49
David Ohhana <sustujbv167@gmail.com>
2025-06-23 09:46:05 +0300

    adding start spring boot post

--- src/components/Posts/posts.config.ts
+++ src/components/Posts/posts.config.ts
@@ -21,5 +21,19 @@ export const posts: IPostsModel[] = [
         linkUrl: 'https://www.linkedin.com/posts/david-ohhana-backend_%D7%96%D7%94-%D7%A7%D7%A8%D7%94-%D7%90%D7%97%D7%A8%D7%99-%D7%97%D7%95%D7%93%D7%A9%D7%99%D7%9D-%D7%A9%D7%9C-%D7%9C%D7%9E%D7%99%D7%93%D7%94-%D7%A2%D7%91%D7%95%D7%93%D7%94-%D7%A7%D7%A9%D7%94-activity-7338096091650813953-PS9f?utm_source=share&utm_medium=member_desktop&rcm=ACoAADgzz3IBxmiPzqHz3QAlnw2HNVT-B2PIMa0',
         color: `#2978C6`
     },
+{
+id: 2,
+title: {
+    en: 'So, How we Start Spring Boot project?',
+    he: 'אז איך מתחילים פרויקט Spring Boot?',
+
+},
+description: {
+    en: ' A short post explaining how to start a Spring Boot project?',
+    he: 'פוסט קצר המסביר איך בכלל מתחילים פרויקט Spring Boot? ',
+},
+    linkUrl: 'https://www.linkedin.com/posts/david-ohhana-backend_%D7%90%D7%99%D7%9A-%D7%9C%D7%99%D7%A6%D7%A8-%D7%A4%D7%A8%D7%95%D7%99%D7%99%D7%A7%D7%98-%D7%91spring-activity-7342793539640045569-3Nv_?utm_source=share&utm_medium=member_desktop&rcm=ACoAADgzz3IBxmiPzqHz3QAlnw2HNVT-B2PIMa0',
+    color: '#61DAFB'
+},
     
 ];
